d7c6bf12b33e818bc214f1c08f508c1a8037a981
Filippo Valiante Filho <39290223+filippovf@users.noreply.github.com>
2023-11-29 02:15:27 -0300

    Update INTcount.sh
    Added comments

--- INTcount.sh
+++ INTcount.sh
@@ -2,5 +2,9 @@
 #Script de 1 linha para monitoramento de interrupções.
 #1 line script for INT monitoring.
 #Prof. Filippo Valiante Filho - http://prof.valiante.info.
+#Se não funcionar como esperado tente...
+#If doesn't work as expected try...
+#watch 'vmstat -s | grep nte'
+#watch 'vmstat -s | grep context'
 
 watch 'vmstat -s | grep int'
